af4069966ed37f897c9909bcb44edc09b4cab8de
NIVEDNRAJ <nivednraj12@gmail.com>
2024-08-30 17:40:20 +0530

    audionix

---
+++ lib/presentation/intro/pages/get_started.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../../common/widgets/button/basic_app_button.dart';
+import '../../../core/configs/assets/app_images.dart';
+import '../../../core/configs/assets/app_vectors.dart';
+import '../../../core/configs/theme/app_colors.dart';
+import '../../choose_mode/pages/choose_mode.dart';
+
+class GetStartedPage extends StatelessWidget {
+  const GetStartedPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(
+              vertical: 40,
+              horizontal: 40
+            ),
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.fill,
+                image: AssetImage(
+                  AppImages.introBG,
+                )
+              )
+            ),
+          ),
+
+          Container(
+            color: Colors.black.withOpacity(0.15),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 40,
+              horizontal: 40
+            ),
+            child: Column(
+                children: [
+                  Align(
+                    alignment: Alignment.topCenter,
+                    child: SvgPicture.asset(
+                      AppVectors.logo
+                    ),
+                  ),
+                  const Spacer(),
+                  const Text(
+                    'Enjoy Listening To Music',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 18
+                    ),
+                  ),
+                  const SizedBox(height: 21,),
+                   const Text(
+                    'Music fuels the soul, energizes the mind, and connects the world. Dive into the rhythm, embrace the melody, and let AUDIONIX be your guide on this sonic journey.',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      color: AppColors.grey,
+                      fontSize: 13
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 20,),
+                  BasicAppButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (BuildContext context) => const ChooseModePage()
+                        )
+                     );
+                    },
+                    title: 'Get Started'
+                  )
+                ],
+              ),
+          ),
+        ],
+      ),
+    );
+  }
+}
